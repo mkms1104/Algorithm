58d59dd6657daa195a10083f216787473efff5e9
mskim <mkms1104@naver.com>
2025-09-11 16:14:41 +0900

    feat: add solutions for LeetCode 104 and 872
    - 104: Maximum Depth of Binary Tree
    - 872: Leaf-Similar Trees

---
+++ src/main/kotlin/leetCode/bfs/_104.java
@@ -0,0 +1,36 @@
+package leetCode.bfs;
+
+public class _104 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int output = new _104().maxDepth(root);
+        System.out.println(output);
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
---
+++ src/main/kotlin/leetCode/bfs/_872.java
@@ -0,0 +1,54 @@
+package leetCode.bfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _872 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new _872.TreeNode(1);
+        TreeNode root2 = new _872.TreeNode(1);
+//        root.left = new _872.TreeNode(5);
+//        root.left.left = new _872.TreeNode(6);
+//        root.left.right = new _872.TreeNode(2);
+//        root.left.right.left = new _872.TreeNode(7);
+//        root.left.right.right = new _872.TreeNode(4);
+//        root.right = new _872.TreeNode(1);
+//        root.right.left = new _872.TreeNode(9);
+//        root.right.right = new _872.TreeNode(8);
+
+        boolean output = new _872().leafSimilar(root1, root2);
+        System.out.println(output);
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+
+        return leaves1.equals(leaves2);
+    }
+
+    private void dfs(TreeNode node, List<Integer> leaves) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+        }
+        dfs(node.left, leaves);
+        dfs(node.right, leaves);
+    }
+}
