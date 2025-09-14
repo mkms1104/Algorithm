b3815e2fe204595df72f8ea13a09ccfa18a03943
mskim <mkms1104@naver.com>
2025-09-14 23:24:49 +0900

    feat: add solutions for LeetCode 1448
    - 1448: Count Good Nodes in Binary Tree

---
+++ src/main/kotlin/leetCode/bfs/_1448.java
@@ -0,0 +1,58 @@
+package leetCode.bfs;
+
+public class _1448 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int count = 0;
+    private int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(3);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+
+        int output = new _1448().goodNodes(root);
+        System.out.println(output);
+    }
+
+    public int goodNodes(TreeNode root) {
+
+        dfs(root);
+        return count;
+    }
+
+    // DFS 탐색 시작
+    // max = Integer.MIN_VALUE로 초기화
+    // max 값과 현재 노드 값을 비교해서 현재 노드 값이 크면 count + 1
+    // 다음 노드가 null이 아닌 경우 max 값을 현재 노드 값으로 교체
+    public void dfs(TreeNode node) {
+        if(node == null) return;
+
+        // 현재 max 값 백업
+        int prevMax = max;
+
+        if(node.val >= max) {
+            count++;
+            max = node.val;
+        }
+
+        dfs(node.left);
+        dfs(node.right);
+
+        max = prevMax;
+    }
+}
